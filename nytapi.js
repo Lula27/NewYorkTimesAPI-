29c8ccc76217dfd659097ad0b944366773b3b987
Loretta Agyemang <Loretta Agyemang>
2016-12-20 16:03:04 -0500

    AJAX call placed in button

--- nytapi.js
+++ nytapi.js
@@ -5,56 +5,46 @@
 
 $(document).ready(function() { 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; 
-url += '?' + $.param({
-	'api-key': "9f47960a4f5d4a55b1a483a4b1a9524d"
-}); 
-
-
-// AJAX call 
-$.ajax({
-	url: url,
-	method: 'GET', 
-}).done(function(result) {
 
 // Log result 
 // console.log(result); 
 // console.log(response); 
 // console.log(result.response.docs[0]);
 
-var article = result.response.docs
-
-$("#output").html(article); 
-
-
-	}); 
 
 	// With click on search button 
 	$("#searchButton").on("click", function() { 
-			$.ajax({
-				url: url,
-				method: 'GET', 
-			}).done(function(result) {
+			
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; 
+		url += '?' + $.param({
+		'api-key': "9f47960a4f5d4a55b1a483a4b1a9524d"
+		}); 
 
-			var article = result.response.docs[0]
+
+	// AJAX call 
+		$.ajax({ 
+			url: url,
+			method: 'GET', 
+		}).done(function(result) {  
+
+
+			var article = result.response.docs[0, 1, 2]; 
 
 			// $("#output").append(article); 
 
 			 console.log(article);
 			 // console log works with button push!  
 
-			 // $("<div>").attr("src", url);
+			 $("<div>").attr("src", url);
 			 // results in Error 429 
 
-			// $("#output").prepend(article); 
+			 $("#output").prepend(article); 
 
 		});
 
-
-
-
-	}); 
-
-}); 
+	});  
+	
+});
+	
 
 	
